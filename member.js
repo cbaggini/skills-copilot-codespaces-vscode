107e23e2ffb37aa3e7a4beb8ab9e05baffbddd2e
Cecilia Baggini <59641982+cbaggini@users.noreply.github.com>
2024-05-16 11:52:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,75 @@
+function skillsMember() {
+    var skills = [
+        {
+            name: 'HTML5',
+            image: 'html5.png',
+            level: 4
+        },
+        {
+            name: 'CSS3',
+            image: 'css3.png',
+            level: 4
+        },
+        {
+            name: 'JavaScript',
+            image: 'javascript.png',
+            level: 3
+        },
+        {
+            name: 'jQuery',
+            image: 'jquery.png',
+            level: 3
+        },
+        {
+            name: 'AngularJS',
+            image: 'angularjs.png',
+            level: 2
+        },
+        {
+            name: 'ReactJS',
+            image: 'reactjs.png',
+            level: 1
+        },
+        {
+            name: 'NodeJS',
+            image: 'nodejs.png',
+            level: 1
+        },
+        {
+            name: 'PHP',
+            image: 'php.png',
+            level: 3
+        },
+        {
+            name: 'MySQL',
+            image: 'mysql.png',
+            level: 3
+        },
+        {
+            name: 'MongoDB',
+            image: 'mongodb.png',
+            level: 1
+        },
+        {
+            name: 'Photoshop',
+            image: 'photoshop.png',
+            level: 2
+        },
+        {
+            name: 'Illustrator',
+            image: 'illustrator.png',
+            level: 2
+        },
+        {
+            name: 'Sketch',
+            image: 'sketch.png',
+            level: 1
+        },
+        {
+            name: 'Git',
+            image: 'git.png',
+            level: 3
+        }
+    ];
+    return skills;
+}
